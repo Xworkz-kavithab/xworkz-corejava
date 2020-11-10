a8ea1d620b9e99cdfb9f7cb4ce37e42bbfbba5b6
Xworkz-kavithab <bkavithashree23@gmail.com>
2020-11-10 11:41:03 +0530

    Adding:Bar file

---
+++ Bar.java
@@ -0,0 +1,10 @@
+class Bar
+{
+  public static void main(String a[])
+  {
+    String mentos[]={"moctail","30","60"};
+	for(int mentosValue=0;mentosValue<=2;mentosValue++)
+	System.out.println(mentos[mentosValue]);
+	}
+}
+	
